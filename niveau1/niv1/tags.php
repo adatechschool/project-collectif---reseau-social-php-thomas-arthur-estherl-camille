be48ac5eff56c848a3db30d325c9ac5d50d65123
EstherL6 <91962646+EstherL6@users.noreply.github.com>
2022-02-10 16:47:43 +0100

    fin du niveau 1

--- niveau1/niv1/tags.php
+++ niveau1/niv1/tags.php
@@ -97,27 +97,25 @@
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                        <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address>par <?php echo $post['author_name'] ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
+                            <p><?php echo $post['content'] ?></p>
                             <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p>#<?php echo $post['taglist'] ?></p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small>♥ <?php echo $post['like_number'] ?></small>
+                            <a href="">#<?php echo $post['taglist'] ?></a>,
                         </footer>
                     </article>
                 <?php } ?>
+                
 
 
             </main>
