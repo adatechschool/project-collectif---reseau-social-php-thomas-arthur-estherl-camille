4f6f2d2af785d8edb8dd8fab1d5c7c2dac94f70c
Camille <folcamille@yahoo.fr>
2022-02-10 12:05:03 +0100

    page admin fini et début page settings

--- niveau1/niv1/settings.php
+++ niveau1/niv1/settings.php
@@ -26,15 +26,12 @@
             </nav>
         </header>
         <div id="wrapper" class='profile'>
-
-
             <aside>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les informations de l'utilisatrice
                         n° <?php echo intval($_GET['user_id']) ?></p>
-
                 </section>
             </aside>
             <main>
