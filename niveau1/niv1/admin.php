fa4e0a03175e59a1577cb7fe9a9b57756227b43e
Camille <folcamille@yahoo.fr>
2022-02-15 16:22:04 +0100

    Profil pour la connexion et ajout des sessions start

--- niveau1/niv1/admin.php
+++ niveau1/niv1/admin.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html lang="fr">
     <head>
@@ -12,23 +15,24 @@
             <img src="resoc.jpg" alt="Logo de notre réseau social"/>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
+                <a href="wall.php?user_id=<?php echo $_SESSION['connected_id']  ?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $_SESSION['connected_id']  ?>">Flux</a>
                 <a href="tags.php?tag_id=1">Mots-clés</a>
                 <a href="login.php">Login</a>
             </nav>
             <nav id="user">
                 <a href="#">Profil</a>
                 <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id']  ?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id']  ?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id']  ?>">Mes abonnements</a></li>
                 </ul>
 
             </nav>
         </header>
 
         <?php
+        $userId =intval($_GET['user_id']);
         /**
          * Etape 1: Ouvrir une connexion avec la base de donnée.
          */
